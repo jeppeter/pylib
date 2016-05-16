7b5e6d2dc7de555f5976068fb0b850700a3e2e9e
jeppeter <jeppeter@gmail.com>
2016-05-16 17:45:36 +0800

    to makemini.sh

---
+++ makemini.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python3 pylib/dpkg/dpkgrm.py -vvvv exrm ubuntu-minimal rsyslog linux-image-extra-`uname -r` network-manager vim openssh-server cron irqbalance grub-pc logrotate dconf-gsettings-backend
