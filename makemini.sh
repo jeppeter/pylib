8fcee874021661c552229abd0f1fcecb161a0977
jeppeter <jeppeter@gmail.com>
2016-05-18 14:34:35 +0800

    to add squashfs-tools into reserved packages

--- makemini.sh
+++ makemini.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-python3 pylib/dpkg/dpkgrm.py -vvvv --root /home/wrc/inst exrm ubuntu-minimal rsyslog linux-image-extra-`uname -r` network-manager vim openssh-server cron irqbalance grub-pc logrotate dconf-gsettings-backend bind9-host man-db libc-bin cifs-utils linux-firmware bash bash-completion
-#python3 pylib/dpkg/dpkgrm.py -vvvv exrm ubuntu-minimal rsyslog linux-image-extra-`uname -r` network-manager vim openssh-server cron irqbalance grub-pc logrotate dconf-gsettings-backend bind9-host man-db libc-bin cifs-utils linux-firmware bash bash-completion
+python3 pylib/dpkg/dpkgrm.py -vvvv --root /home/wrc/inst exrm ubuntu-minimal rsyslog linux-image-extra-`uname -r` network-manager vim openssh-server cron irqbalance grub-pc logrotate dconf-gsettings-backend bind9-host man-db libc-bin cifs-utils linux-firmware bash bash-completion squashfs-tools
+#python3 pylib/dpkg/dpkgrm.py -vvvv exrm ubuntu-minimal rsyslog linux-image-extra-`uname -r` network-manager vim openssh-server cron irqbalance grub-pc logrotate dconf-gsettings-backend bind9-host man-db libc-bin cifs-utils linux-firmware bash bash-completion squashfs-tools
