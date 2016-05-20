69fba40727f6eff1659acfc89365c3f71850277c
jeppeter <jeppeter@gmail.com>
2016-05-20 15:42:27 +0800

    to make pkgs remove

---
+++ removepkgs.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python3 pylib/dpkg/dpkgrm.py -vvvv --root /home/wrc/source/rescueiso/iso/ubdesk_inst/ rm libgnome-menu-3-0 cups-common  gir1.2-ebookcontacts-1.2 libx11-6 libgnome-keyring0 libqtcore4 libunity9 gir1.2-glib-2.0 qtcore4-l10n
