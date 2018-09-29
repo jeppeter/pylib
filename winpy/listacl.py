f2776660cc853db18b1f6b8f2237451f7377281e
jeppeter <jeppeter@gmail.com>
2018-09-29 17:45:11 +0800

    to make all things more

--- winpy/listacl.py
+++ winpy/listacl.py
@@ -30,9 +30,25 @@ def enable_priv():
 
 
 def list_acl(fname):
+  rets = ''
+  owner = win32security.GetNamedSecurityInfo(fname, win32security.SE_FILE_OBJECT, win32security.OWNER_SECURITY_INFORMATION)
+  if owner is not None:
+    (username,domain,nameuse) = win32security.LookupAccountSid(None,owner.GetSecurityDescriptorOwner())
+    if domain is not None and len(domain) > 0:
+      rets += '    owner %s\\%s\n'%(domain,username)
+    else:
+      rets += '    owner %s\n'%(username)
+  group = win32security.GetNamedSecurityInfo(fname, win32security.SE_FILE_OBJECT, win32security.GROUP_SECURITY_INFORMATION)
+  if group is not None:
+    (groupname,domain, nameuse) = win32security.LookupAccountSid(None,group.GetSecurityDescriptorGroup())
+    if domain is not None and len(domain) > 0:
+      rets += '    group %s\\%s\n'%(domain,groupname)
+    else:
+      rets += '    group %s\n'%(groupname)
+  else:
+    rets += '    group none\n'
   dacl = win32security.GetNamedSecurityInfo(fname, win32security.SE_FILE_OBJECT,
       win32security.DACL_SECURITY_INFORMATION).GetSecurityDescriptorDacl()
-  rets = ''
   if dacl is not None:
     for i in range(dacl.GetAceCount()):
       ace = dacl.GetAce(i)
@@ -42,11 +58,18 @@ def list_acl(fname):
       else:
         mask, otype,itype , sid = ace[1:]
       name, domain,ptype = win32security.LookupAccountSid(None,sid)
-      rets += '    %s %s\\%s %x\n'%(CONVENTIONAL_ACES.get(atype,"OTHER"), domain, name,mask)
+      rets += '    DACL %s %s\\%s %x\n'%(CONVENTIONAL_ACES.get(atype,"OTHER"), domain, name,mask)
   sacl = win32security.GetNamedSecurityInfo(fname, win32security.SE_FILE_OBJECT, win32security.SACL_SECURITY_INFORMATION).GetSecurityDescriptorSacl()
   if sacl is not None:
     for i in range(sacl.GetAceCount()):
-      rets += '\n'
+      ace = sacl.GetAce(i)
+      (atype, aflag) = ace[0]
+      if atype in CONVENTIONAL_ACES:
+        mask , sid = ace[1:]
+      else:
+        mask, otype,itype , sid = ace[1:]
+      name, domain,ptype = win32security.LookupAccountSid(None,sid)
+      rets += '    SACL %s %s\\%s %x\n'%(CONVENTIONAL_ACES.get(atype,"OTHER"), domain, name,mask)
   return rets
 
 def main():
