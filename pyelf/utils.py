c3df3937ce9b81b3bfca6a70f32419e0de2f522c
jeppeter <jeppeter@gmail.com>
2022-07-06 15:51:02 +0800

    to make backtracedump ok

--- pyelf/utils.py
+++ pyelf/utils.py
@@ -2,10 +2,20 @@
 
 import logging
 import extargsparse
-import elftools
 import re
 import sys
 
+from elftools.elf.elffile import ELFFile
+
+from elftools.elf.sections import (
+    NoteSection, SymbolTableSection, SymbolTableIndexSection
+)
+
+from elftools.elf.descriptions import (
+	describe_symbol_shndx
+)
+
+from elftools.elf.constants import SHN_INDICES
 
 def set_logging(args):
     loglvl= logging.ERROR
@@ -84,12 +94,73 @@ def write_file_bytes(sarr,outfile=None):
     return 
 
 
+def sort_symbol(k):
+	sym = k[0]
+	return sym['st_value']
+
 class ElfSearchObj(object):
 	def __init__(self,fname):
 		self.__fname = fname
+		self.__elffile = ELFFile(open(fname,'rb'))
+		self.__symtabs = []
+		self.__sections = []
+		sectidx = 0
+		for s in self.__elffile.iter_sections():
+			if isinstance(s,SymbolTableSection):
+				self.__sections.append(s)
+				symidx = 0
+				for symbol in s.iter_symbols():
+					if symbol['st_size'] != 0:
+						nc = (symbol,symidx,sectidx)
+						self.__symtabs.append(nc)
+					symidx += 1
+			sectidx += 1
+		self.__symtabs.sort(key=sort_symbol)
+		self.__shn_sections = []
+		for s in self.__elffile.iter_sections():
+			if isinstance(s,SymbolTableIndexSection):
+				logging.info('SymbolTableIndexSection %s'%(repr(s)))
+				self.__shn_sections.append(s.symboltable)
 		return
 
+	def __search_name(self,sym,symidx,sectidx):
+		return sym.name
+
 	def search_symbol_vaddr(self,offset):
+		sidx = 0
+		eidx = len(self.__symtabs) - 1
+		cidx = int((sidx + eidx)/2)
+		#logging.info('search [%s] [0x%x]'%(self.__fname, offset))
+		while sidx < eidx:
+			k = self.__symtabs[cidx]
+			sym = k[0]
+			if sym['st_value'] <= offset and offset < (sym['st_value'] + sym['st_size']):
+				return '%s+0x%x'%(self.__search_name(sym,k[1],k[2]), offset - sym['st_value'])
+			elif offset < sym['st_value']:
+				eidx = cidx
+			else:
+				sidx = cidx
+
+			if eidx <= sidx:
+				return 'must 0x%x value [0x%x]'%(offset,sym['st_value'])
+
+			if eidx == (sidx + 1):
+				k = self.__symtabs[sidx]
+				sym = k[0]
+				if sym['st_value'] <= offset and offset < (sym['st_value'] + sym['st_size']):
+					return '%s+0x%x'%(self.__search_name(sym,k[1],k[2]), offset - sym['st_value'])
+
+				k = self.__symtabs[eidx]
+				sym = k[0]
+				if sym['st_value'] <= offset and offset < (sym['st_value'] + sym['st_size']):
+					return '%s+0x%x'%(self.__search_name(sym,k[1],k[2]), offset - sym['st_value'])
+				t = self.__symtabs[sidx]
+				tsym = t[0]
+				logging.error('%s [0x%x] + [0x%x] >> 0x%x << %s [0x%x] '%(self.__search_name(tsym,t[1],t[2]), tsym['st_value'] , tsym['st_size'] ,offset,self.__search_name(sym,k[1],k[2]), sym['st_value']))
+				return '%s+0x%x'%(self.__search_name(tsym,t[1],t[2]), offset - tsym['st_value'])
+			#logging.info('sidx [%d] eidx [%d]'%(sidx, eidx))
+			cidx = int((eidx + sidx)/2)
+
 		return ''
 
 
@@ -120,7 +191,7 @@ def backtraceparse_handler(args,parser):
 							if fname not in elfdicts.keys():
 								elfdicts[fname] = ElfSearchObj(fname)
 							funcsym = elfdicts[fname].search_symbol_vaddr(offset)
-							logging.info('[%d]%s search [0x%x]'%(lidx,fname,offset))
+							logging.info('[%d]%s search [0x%x] [%s]'%(lidx,fname,offset, funcsym))
 							rets += '%s %s\n'%(l,funcsym)
 						else:
 							rets += '%s %s\n'%(l,m[0][2])
