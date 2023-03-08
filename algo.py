e1994d6a5620374c4641e95df58dba21593e17ff
jeppeter <jeppeter@gmail.com>
2023-03-08 15:06:41 +0800

    to add algo.py

---
+++ algo.py
@@ -0,0 +1,111 @@
+#! /usr/bin/env python
+
+import extargsparse
+import sys
+import os
+import logging
+import re
+import shutil
+import logging.handlers
+import time
+import struct
+import inspect
+import json
+
+
+def set_logging(args):
+    loglvl= logging.ERROR
+    if args.verbose >= 3:
+        loglvl = logging.DEBUG
+    elif args.verbose >= 2:
+        loglvl = logging.INFO
+    curlog = logging.getLogger(args.lognames)
+    #sys.stderr.write('curlog [%s][%s]\n'%(args.logname,curlog))
+    curlog.setLevel(loglvl)
+    if len(curlog.handlers) > 0 :
+        curlog.handlers = []
+    formatter = logging.Formatter('%(asctime)s:%(filename)s:%(funcName)s:%(lineno)d<%(levelname)s>\t%(message)s')
+    if not args.lognostderr:
+        logstderr = logging.StreamHandler()
+        logstderr.setLevel(loglvl)
+        logstderr.setFormatter(formatter)
+        curlog.addHandler(logstderr)
+
+    for f in args.logfiles:
+        flog = logging.FileHandler(f,mode='w',delay=False)
+        flog.setLevel(loglvl)
+        flog.setFormatter(formatter)
+        curlog.addHandler(flog)
+    for f in args.logappends:       
+        if args.logrotate:
+            flog = logging.handlers.RotatingFileHandler(f,mode='a',maxBytes=args.logmaxbytes,backupCount=args.logbackupcnt,delay=0)
+        else:
+            sys.stdout.write('appends [%s] file\n'%(f))
+            flog = logging.FileHandler(f,mode='a',delay=0)
+        flog.setLevel(loglvl)
+        flog.setFormatter(formatter)
+        curlog.addHandler(flog)
+    return
+
+def load_log_commandline(parser):
+    logcommand = '''
+    {
+        "verbose|v" : "+",
+        "logname" : "root",
+        "logfiles" : [],
+        "logappends" : [],
+        "logrotate" : true,
+        "logmaxbytes" : 10000000,
+        "logbackupcnt" : 2,
+        "lognostderr" : false
+    }
+    '''
+    parser.load_command_line_string(logcommand)
+    return parser
+
+def parse_int(v):
+    c = v
+    base = 10
+    if c.startswith('0x') or c.startswith('0X') :
+        base = 16
+        c = c[2:]
+    elif c.startswith('x') or c.startswith('X'):
+        base = 16
+        c = c[1:]
+    return int(c,base)
+
+
+def ackman(i,j):
+    logging.info('i %d j %d'%(i,j))
+    if i == 0:
+        return j + 1
+    if j == 0:
+        return ackman(i-1,1)
+    return ackman(i-1,ackman(i,j-1))
+
+def ackman_handler(args,parser):
+    set_logging(args)
+    i = parse_int(args.subnargs[0])
+    j = parse_int(args.subnargs[1])
+    sys.stdout.write('ackman (%d , %d) = %d\n'%(i,j,ackman(i,j)))
+    sys.exit(0)
+    return
+
+
+def main():
+    commandline='''
+    {
+        "ackman<ackman_handler>##i j to calculate ackman##" :  {
+            "$" : 2
+        }
+    }
+    '''
+    parser = extargsparse.ExtArgsParse()
+    parser.load_command_line_string(commandline)
+    load_log_commandline(parser)
+    parser.parse_command_line(None,parser)
+    raise Exception('can not reach here')
+    return
+
+if __name__ == '__main__':
+    main()    
