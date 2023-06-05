79e8f0c96ec7b38535bb8b2389b5eb273b2c47e2
jeppeter <jeppeter@gmail.com>
2023-06-05 09:03:17 +0800

    to add X_PARAM Y_PARAM

--- algo/ecc/ecbase.py
+++ algo/ecc/ecbase.py
@@ -6,6 +6,8 @@ import logging
 A_PARAM = 'a'
 B_PARAM = 'b'
 ORDER_PARAM = 'order'
+X_PARAM = 'x'
+Y_PARAM = 'y'
 
 class ECBase(object):
 	def __init__(self,jsons=''):
@@ -17,9 +19,13 @@ class ECBase(object):
 		if A_PARAM not in rdict.keys() or B_PARAM not in rdict.keys() or \
 			ORDER_PARAM not in rdict.keys():
 			raise Exception('no [%s] or [%s] or [%s] in jsons'%(A_PARAM,B_PARAM,ORDER_PARAM))
+		if X_PARAM not in rdict.keys() or Y_PARAM not in rdict.keys():
+			raise Exception('no [%s] or [%s] in jsons'%(X_PARAM,Y_PARAM))
 		self.a = rdict[A_PARAM]
 		self.b = rdict[B_PARAM]
 		self.order = rdict[ORDER_PARAM]
+		self.x = rdict[X_PARAM]
+		self.y = rdict[Y_PARAM]
 		return
 
 	def pack(self):
@@ -27,6 +33,8 @@ class ECBase(object):
 		rdict[A_PARAM] = self.a
 		rdict[B_PARAM] = self.b
 		rdict[ORDER_PARAM] = self.order
+		rdict[X_PARAM] = self.x
+		rdict[Y_PARAM] = self.y
 		return json.dumps(rdict,indent=4)
 
 
@@ -35,3 +43,5 @@ class ECBase(object):
 
 	def __repr__(self):
 		return self.pack()
+
+
