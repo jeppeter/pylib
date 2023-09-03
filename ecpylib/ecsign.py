e98e1ba9f711c5c57d7b9808719a76913b36213b
jeppeter <jeppeter@gmail.com>
2023-09-03 19:20:06 +0800

    to make sig_random

--- ecpylib/ecsign.py
+++ ecpylib/ecsign.py
@@ -12,6 +12,42 @@ import loglib
 import ecdsa
 
 
+class RandomBytes(object):
+	def __init__(self):
+		fname = os.getenv('ECSIMPLE_RANDFILE')
+		self.fname = fname
+		if self.fname is not None:
+			with open(self.fname,'rb'):
+				self.fbytes = b''
+				while True:
+					retb = read(self.fp,1024)
+					if retb is None or len(retb) == 0:
+						break
+					self.fbytes += retb
+			self.curidx = 0
+		else:
+			self.fbytes = None
+			self.curidx = -1
+	def get_bytes(bsize):
+		if self.fbytes is None:
+			retb = os.urandom(bsize)
+		else:
+			if (self.curidx + bsize) < len(self.fbytes):
+				retb=  self.fbytes[self.curidx:(self.curidx + bsize)]
+				self.curidx += bsize
+			else:
+				raise Exception('[%d] + [%d] > [%d]'%(self.curidx,bsize,len(self.fbytes)))
+		return retb
+
+
+DEFAULT_RANDOP=None
+
+def ec_random(bsize):
+	global DEFAULT_RANDOP
+	if DEFAULT_RANDOP is None:
+		DEFAULT_RANDOP = RandomBytes()
+	return DEFAULT_RANDOP.get_bytes(bsize)
+
 
 def ecp256sign_handler(args,parser):
 	loglib.set_logging(args)
@@ -41,14 +77,66 @@ def ecp256gen_handler(args,parser):
 	sk = ecdsa.SigningKey.generate(ecdsa.NIST256p)
 	pk = sk.get_verifying_key()
 	rb  = sk.to_string()
-	rets = fileop.format_bytes(rb,'privatekey')
-	fileop.write_file(rets,None)
+	if args.ecpriv is None:
+		rets = fileop.format_bytes(rb,'privatekey')
+		fileop.write_file(rets,args.ecpriv)
+	else:
+		fileop.write_file_bytes(rb,args.ecpriv)
 	rb =  pk.to_string()
-	rets = fileop.format_bytes(rb,'publickey')
-	fileop.write_file(rets,None)
+	if args.ecpub is None:
+		rets = fileop.format_bytes(rb,'publickey')
+		fileop.write_file(rets,args.ecpub)
+	else:
+		fileop.write_file_bytes(rb,args.ecpub)
 	sys.exit(0)
 	return
 
+
+def ecp224sign_handler(args,parser):
+	loglib.set_logging(args)
+	keyb = fileop.read_file_bytes(args.subnargs[0])
+	conb = fileop.read_file_bytes(args.subnargs[1])
+	sk = ecdsa.SigningKey.from_string(keyb,curve=ecdsa.NIST224p)
+	sig = sk.sign(conb)
+	rets = fileop.format_bytes(sig,'signature')
+	fileop.write_file(rets,None)
+	fileop.write_file_bytes(sig,args.output)
+	sys.exit(0)
+	return
+
+def ecp224vfy_handler(args,parser):
+	loglib.set_logging(args)
+	keyb = fileop.read_file_bytes(args.subnargs[0])
+	conb = fileop.read_file_bytes(args.subnargs[1])
+	signb = fileop.read_file_bytes(args.subnargs[2])
+	pk = ecdsa.VerifyingKey.from_string(keyb,curve=ecdsa.NIST224p)
+	retb = pk.verify(signb,conb)
+	sys.stdout.write('retb %s\n'%(retb))
+	sys.exit(0)
+	return
+
+def ecp224gen_handler(args,parser):
+	loglib.set_logging(args)
+	if len(args.subnargs) > 0:
+		secnum = fileop.parse_int(args.subnargs[0])
+		sk = ecdsa.SigningKey.from_secret_exponent(secnum,ecdsa.NIST224p)
+	else:
+		sk = ecdsa.SigningKey.generate(ecdsa.NIST224p)
+	pk = sk.get_verifying_key()
+	rb  = sk.to_string()
+	if args.ecpriv is None:
+		rets = fileop.format_bytes(rb,'privatekey')
+		fileop.write_file(rets,args.ecpriv)
+	else:
+		fileop.write_file_bytes(rb,args.ecpriv)
+	rb =  pk.to_string()
+	if args.ecpub is None:
+		rets = fileop.format_bytes(rb,'publickey')
+		fileop.write_file(rets,args.ecpub)
+	else:
+		fileop.write_file_bytes(rb,args.ecpub)
+	sys.exit(0)
+	return
 
 
 def main():
@@ -56,6 +144,8 @@ def main():
 	{
 		"input|i" : null,
 		"output|o" : null,
+		"ecpriv" : null,
+		"ecpub" : null,
 		"ecp256sign<ecp256sign_handler>##key.bin content.bin to sign in ecdsa##" : {
 			"$" : 2
 		},
@@ -64,8 +154,16 @@ def main():
 		},
 		"ecp256gen<ecp256gen_handler>##to generate gen##" : {
 			"$" : 0
-		}
-	}
+		},
+		"ecp224sign<ecp224sign_handler>##key.bin content.bin to sign in ecdsa##" : {
+			"$" : 2
+		},
+		"ecp224vfy<ecp224vfy_handler>##key.bin content.bin sign.bin to verify ecdsa##" : {
+			"$" : 3
+		},
+		"ecp224gen<ecp224gen_handler>##to generate gen##" : {
+			"$" : 0
+		}	}
 	'''
 	parser = extargsparse.ExtArgsParse()
 	parser.load_command_line_string(commandline)
