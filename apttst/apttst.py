dc19b0d5de312b2f0e7becb1111650fae47a859c
jeppeter <jeppeter@gmail.com>
2024-02-27 10:53:49 +0800

    to make depmap ok for apt

--- apttst/apttst.py
+++ apttst/apttst.py
@@ -1,16 +1,34 @@
 #! /usr/bin/env python
 
-import extargsparse
 import logging
 import sys
 import os
 import apt
-
-
+import json
+import itertools, operator
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'pythonlib')))
 import fileop
 import logop
+import extargsparse
+
+if sys.hexversion < 0x03000000:
+    mapper= itertools.imap # 2.4 ≤ Python < 3
+else:
+    mapper= map # Python ≥ 3
+
+def sort_uniq(sequence):
+    retv = sorted(sequence)
+    reto = []
+    if len(retv) > 0:
+        reto.append(retv[0])
+    idx = 0
+    while idx < (len(retv) - 1):
+        if retv[idx] != retv[idx+1]:
+            reto.append(retv[idx+1])
+        idx += 1
+    return reto
 
 
 def cachelist_handler(args,parser):
@@ -25,19 +43,51 @@ def depmap_handler(args,parser):
     logop.set_logging(args)
     depmap = dict()
     rdepmap = dict()
+    depfile = None
+    rdepfile = None
+    if len(args.subnargs) > 0:
+        depfile = args.subnargs[0]
+    if len(args.subnargs) > 1:
+        rdepfile = args.subnargs[1]
+
+
     cache = apt.Cache()
     for p in cache:
+        k = '%s'%(p)
         if not hasattr(p, 'candidate'):
-            sys.stdout.write('%s no candidate\n%s\n'%(p,dir(p)))
+            sys.stderr.write('%s no candidate\n%s\n'%(p,dir(p)))
+            if k not in depmap.keys():
+                depmap[k] = []
         else:
             if hasattr(p.candidate, 'dependencies'):
-                sys.stdout.write('%s dependency\n'%(p))
+                #sys.stdout.write('%s dependency\n'%(p))
                 for dps in p.candidate.dependencies:
                     for dp in dps:
-                        sys.stdout.write('    %s type %s\n'%(dp.name,dp.rawtype))
+                        if k not in depmap.keys():
+                            depmap[k] = []
+                        if dp.rawtype == 'Depends':
+                            depmap[k].append(dp.name)
+                        else:
+                            logging.info('%s dp [%s] rawtype [%s]'%(k,dp.name,dp.rawtype))
+                        #sys.stdout.write('    %s type %s\n'%(dp.name,dp.rawtype))
                 #sys.stdout.write('%s dependencies\n%s\n'%(p,p.candidate.dependencies))
             else:
-                sys.stdout.write('%s no candidate.dependencies\n%s\n'%(p,dir(p.candidate)))
+                if k not in depmap.keys():
+                    depmap[k] = []
+                sys.stderr.write('%s no candidate.dependencies\n%s\n'%(p,dir(p.candidate)))
+    for k in depmap.keys():
+        for p in depmap[k]:
+            if p not in rdepmap.keys():
+                rdepmap[p] = []
+            rdepmap[p].append(k)
+    for k in rdepmap.keys():
+        v = sort_uniq(rdepmap[k])
+        rdepmap[k] = v
+
+    s = json.dumps(depmap,indent=4)
+    fileop.write_file(s,depfile)
+    s = json.dumps(rdepmap,indent=4)
+    fileop.write_file(s,rdepfile)
     sys.exit(0)
     return
 
@@ -49,8 +99,8 @@ def main():
         "cachelist<cachelist_handler>##to list cache##" : {
             "$" : 0
         },
-        "depmap<depmap_handler>##to format dep map##" : {
-            "$" : 0
+        "depmap<depmap_handler>## [depmapfile] [rdepmapfile] to format dep map##" : {
+            "$" : "+"
         }
     }
     '''
