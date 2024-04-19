bcd92cc94c75299adfe3581fa43717729cb91f7d
jeppeter <jeppeter@gmail.com>
2024-04-19 14:57:44 +0800

    to make for the fmterrors update code

--- rustutil.py
+++ rustutil.py
@@ -5,25 +5,78 @@ RUST_ERRORS_FORMAT = '''
 #[macro_export]
 macro_rules! %RUST_NAME%_error_class {
 	($type:ident) => {
-	#[derive(Debug,Clone)]
-	pub struct $type {
-		msg :String,		
-	}
-
-	#[allow(dead_code)]
-	impl $type {
-		fn create(c :&str) -> $type {
-			$type {msg : format!("{}",c)}
+		#[derive(Clone)]
+		pub struct $type {
+			msg :String,
+			fname :String,
+			lineno :u32,
+			types :String,
 		}
-	}
 
-	impl std::fmt::Display for $type {
-		fn fmt(&self,f :&mut std::fmt::Formatter) -> std::fmt::Result {
-			write!(f,"{}",self.msg)
+		#[allow(dead_code)]
+		impl $type {
+			fn create(fname :&str,lineno :u32, types :&str,c :&str) -> $type {
+				$type {
+					msg : format!("{}",c),
+					fname : format!("{}",fname),
+					lineno : lineno,
+					types : format!("{}",types),
+				}
+			}
 		}
-	}
 
-	impl std::error::Error for $type {}
+		impl std::fmt::Display for $type {
+			fn fmt(&self,f :&mut std::fmt::Formatter) -> std::fmt::Result {
+				let mut errdisplay : bool =false;
+				match std::env::var("ERROR_LEVEL") {
+					Ok(vs) => {
+						match vs.parse::<i32>() {
+							Ok(v) => {
+								if v >= 3 {
+									errdisplay = true;
+								}
+							},
+							Err(_e) => {
+							}
+						}
+					},
+					_ => {},
+				}
+				if errdisplay {
+					write!(f,"[{}:{}][{}]{}",self.fname,self.lineno,self.types,self.msg)
+				} else {
+					write!(f,"{}",self.msg)	
+				}
+				
+			}
+		}
+
+		impl std::fmt::Debug for $type {
+			fn fmt(&self,f :&mut std::fmt::Formatter) -> std::fmt::Result {
+				let mut errdisplay : bool =false;
+				match std::env::var("ERROR_LEVEL") {
+					Ok(vs) => {
+						match vs.parse::<i32>() {
+							Ok(v) => {
+								if v >= 3 {
+									errdisplay = true;
+								}
+							},
+							Err(_e) => {
+							}
+						}
+					},
+					_ => {},
+				}
+				if errdisplay {
+					write!(f,"[{}:{}][{}]{}",self.fname,self.lineno,self.types,self.msg)
+				} else {
+					write!(f,"{}",self.msg)	
+				}				
+			}
+		}
+
+		impl std::error::Error for $type {}
 	};
 }
 
@@ -31,10 +84,27 @@ macro_rules! %RUST_NAME%_error_class {
 macro_rules! %RUST_NAME%_new_error {
 	($type:ty,$($a:expr),*) => {
 		{
-		let mut c :String= format!("[{}:{}][{}]",file!(),line!(),stringify!($type));
-		c.push_str(&(format!($($a),*)[..]));
-		return Err(Box::new(<$type>::create(c.as_str())));
-	  }
+			let fname = format!("{}",file!());
+			let lineno = line!();
+			let types = format!("{}",stringify!($type));
+			let mut c :String= format!("");
+			c.push_str(&(format!($($a),*)[..]));
+			return Err(Box::new(<$type>::create(&fname,lienno,&types,c.as_str())));
+		}
+	};
+}
+
+
+#[macro_export]
+macro_rules! %RUST_NAME%_error_create {
+	($type:ty,$($a:expr),*) => {
+		{
+			let fname = format!("{}",file!());
+			let types = format!("{}",stringify!($type));
+			let mut c :String= format!("");
+			c.push_str(&(format!($($a),*)[..]));
+			Box::new(<$type>::create(&fname,line!(),&types,c.as_str()))
+		}
 	};
 }
 '''
