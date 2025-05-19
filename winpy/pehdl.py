9eba134b8dd4d7823e5b753bcb7092ac7ebee0b4
jeppeter <jeppeter@gmail.com>
2025-05-19 16:12:57 +0800

    to add pemapsearch_handler

--- winpy/pehdl.py
+++ winpy/pehdl.py
@@ -5,6 +5,7 @@ import re
 import sys
 import os
 import logging
+import struct
 
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
@@ -71,7 +72,7 @@ def callsearch_handler(args,parser):
 	sarr = re.split('\n',s)
 	funcstack = []
 	funcexpr = re.compile('^\\[([0-9]+)\\]\\s+0x([0-9a-fA-F]+)$')
-	spanexpr = re.compile('^\\[0x([0-9a-fA-F]+)\\]\\s+\\-\\s+\\[0x([0-9a-fA-F]+)\\]\\s+\\[([^\\]]*)\\]')
+	spanexpr = re.compile('^\\[0x([0-9a-fA-F]+)\\]\\s+\\-\\s+\\[0x([0-9a-fA-F]+)\\]\\s+\\[([^\\]]*)\\]\\s+\\[([^\\]]*)\\]')
 	memmap = MemoryMap()
 
 	for l in sarr:
@@ -81,12 +82,109 @@ def callsearch_handler(args,parser):
 			funcstack.append(int(m[0][1],16))
 			continue
 		m = spanexpr.findall(l)
-		if m is not None and len(m) > 0 and len(m[0]) > 2:
-			memmap.add_map(int(m[0][0],16),int(m[0][1],16),m[0][2])
+		if m is not None and len(m) > 0 and len(m[0]) > 3:
+			memmap.add_map(int(m[0][0],16),int(m[0][1],16),m[0][3])
 			continue
 	for f in funcstack:
 		saddr ,offset, fname = memmap.search(f)
 		sys.stdout.write('[0x%x] 0x%x 0x%x %s\n'%(f,saddr,offset,fname))
+	sys.exit(0)
+	return
+
+class OffsetFile(object):
+	def __init__(self,fname,memoff,memsize):
+		self.fname = fname
+		self.memoff = memoff
+		self.memsize = memsize
+		self.data = []
+		return
+
+	def add_bytes(self,data):
+		self.data.extend(data)
+		return
+	def cmp_bytes(self,other):
+		idx = 0
+		offset = 0
+		msize = 0
+		sidx = 0
+		didx = 0
+		moffset = -1
+
+		if len(self.data) < 16:
+			raise Exception('self.data [%d]'%(len(self.data)))
+		if len(other.data) < 16:
+			raise Exception('other.data [%d]'%(len(other.data)))
+		while didx < len(other.data):
+			tmpdidx = didx
+			sidx = 0
+			# first to search 16 bytes
+			while sidx < 16 and tmpdidx < len(other.data):
+				if self.data[sidx] != other.data[tmpdidx]:
+					break
+				sidx += 1
+				tmpdidx += 1
+			if sidx < 16:
+				didx += 1
+				continue
+			# now it is match
+			while sidx < len(self.data) and tmpdidx < len(other.data):
+				if self.data[sidx] != other.data[tmpdidx]:
+					break
+				sidx += 1
+				tmpdidx += 1
+			logging.info('curoffset 0x%x  size 0x%x'%(tmpdidx - sidx, sidx))
+			if sidx > msize:
+				moffset= tmpdidx - sidx
+				msize = sidx
+			didx += sidx			
+		return moffset,msize
+
+def pemapsearch_handler(args,parser):
+	loglib.set_logging(args)
+	s =fileop.read_file(args.input)
+	sarr = re.split('\n',s)
+	funcstack = []
+	startfileexpr = re.compile('^\\[([0-9]+)\\]\\[([^\\]]+)\\]\\s+0x([0-9a-fA-F]+)\\s+size\\s+0x([0-9a-fA-F]+)')
+	linematchexpr = re.compile('^0x([0-9a-fA-F]+):\\s+(.*)')
+	mfile = False
+	curfile = None
+	offsetmaps = []
+	for l in sarr:
+		l = l.rstrip('\r')
+		if curfile is None:
+			m = startfileexpr.findall(l)
+			if m is not None and len(m) > 0:
+				curfile = OffsetFile(m[0][1],int(m[0][2],16),int(m[0][3],16))
+				continue
+		else:
+			m = linematchexpr.findall(l)
+			if m is not None and len(m) > 0 and len(m[0]) > 1:
+				#logging.info('l[%s]'%(l))
+				carr = re.split('\\s+',m[0][1])
+				idx = 0
+				jdx = 0
+				data = []
+				while idx < 16:
+					if len(carr[jdx]) > 0:
+						data.append(int(carr[jdx][2:],16))
+						idx += 1
+					jdx += 1
+				curfile.add_bytes(data)
+			else:
+				offsetmaps.append(curfile)
+				curfile = None
+			continue
+
+	for f in args.subnargs:
+		basef = os.path.basename(f)
+		fdata = fileop.read_file_bytes(f)
+		rawdata = OffsetFile(f,0,0)
+		rawdata.add_bytes(fdata)
+		for m in offsetmaps:
+			cmpf = os.path.basename(m.fname)
+			if cmpf == basef:
+				offset,size = m.cmp_bytes(rawdata)
+				sys.stdout.write('[%s][0x%x][0x%x] match 0x%x size 0x%x\n'%(m.fname,m.memoff,m.memsize,offset,size))
 	sys.exit(0)
 	return
 
@@ -97,6 +195,9 @@ def main():
 		"output|o" : null,
 		"callsearch<callsearch_handler>##from input to search file##" : {
 			"$" : 0
+		},
+		"pemapsearch<pemapsearch_handler>##dllfile ... to map files##" : {
+			"$" : "+"
 		}
 	}
 	'''
